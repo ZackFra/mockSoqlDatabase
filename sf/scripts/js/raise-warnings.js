7541fc205cd47986871a0a1c7a837ed9dfb4c938
Zackary Frazier <zack11frazier22@gmail.com>
2024-02-02 21:52:17 -0800

    Refactor code to use codeScanFile instead of warningFile

--- sf/scripts/js/raise-warnings.js
+++ sf/scripts/js/raise-warnings.js
@@ -6,22 +6,20 @@ const MAX_MESSAGES = 10;
 const MAX_SEVERITY = 1;
 const MIN_SEVERITY = 5;
 
-const errors = [];
-const warningFile = JSON.parse(fs.readFileSync('codescan.json', 'utf8'));
+const codeScanFile = JSON.parse(fs.readFileSync('codescan.json', 'utf8'));
 
 const errorMap = {};
 for(let i = 1; i <= 5; i++) {
     errorMap[i] = [];
 }
 
-for(const warning of warningFile) {
+for(const warning of codeScanFile) {
     for(const violation of warning.violations) {
         let message = `::warning file=${warning.fileName},line=${violation.line},endLine=${violation.endLine},title=${violation.ruleName}::SEVERITY (${violation.severity}) ${violation.message}`
         message = message.replaceAll('\r','').replaceAll('\n','');
 
         // group severity messages to prioritize higher severity messages
         errorMap[violation.severity].push(message);
-        errors.push(message);
     };
 }
 
