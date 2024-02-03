474ac918e9efd8b252297c3f5099addfe96b51a3
Zackary Frazier <zack11frazier22@gmail.com>
2024-02-02 21:40:56 -0800

    Refactor raise-warnings.js to prioritize higher severity messages

--- sf/scripts/js/raise-warnings.js
+++ sf/scripts/js/raise-warnings.js
@@ -1,16 +1,40 @@
 // `::warning file={name},line={line},endLine={endLine},title={title}::{message}`
 
 const fs = require('fs');
+
+const MAX_MESSAGES = 10;
+
 const errors = [];
-JSON.parse(fs.readFileSync('codescan.json', 'utf8'))
-.forEach((warning) => {
-    warning.violations.forEach((violation) => {
+const warningFile = JSON.parse(fs.readFileSync('codescan.json', 'utf8'));
+
+const errorMap = {};
+for(let i = 1; i <= 5; i++) {
+    errorMap[i] = [];
+}
+
+for(const warning of warningFile) {
+    for(const violation of warning.violations) {
         let message = `::warning file=${warning.fileName},line=${violation.line},endLine=${violation.endLine},title=${violation.ruleName}::SEVERITY (${violation.severity}) ${violation.message}`
         message = message.replaceAll('\r','').replaceAll('\n','');
+        // prioritize higher severity since GitHub actions max out at 10 warnings
+        errorMap[violation.severity].push(message);
         errors.push(message);
-    });
-});
-errors.forEach((error) => {
-    console.log(error);
-});
+    };
+}
 
+let currSeverity = 1;
+let totalMessages = 0;
+
+let hasMessage = (errorMap[currSeverity].length > 0);
+let canPrintMoreMessages = (totalMessages <= MAX_MESSAGES);
+while(hasMessage && canPrintMoreMessages) {
+    console.log(errorMap[i].shift());
+
+    totalMessages++;
+    if(totalMessages === 10) {
+        i++;
+    }
+
+    hasMessage = (errorMap[currSeverity].length > 0);
+    canPrintMoreMessages = (totalMessages <= MAX_MESSAGES);
+}
